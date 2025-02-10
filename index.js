342f377e1d2253275060b32dc56430e05c5f02a6
Luiz Vinicius Lima da Silva <luizviniciuslimadasilva@gmail.com>
2025-02-10 17:25:00 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+function calcularMelhorPreco() {
+    
+    let precoAlcool = document.getElementById('alcool').value
+    let precoGasolina = document.getElementById('gasolina').value
+
+    if( precoAlcool != "" ){
+        if( precoGasolina != "" ){
+
+            let resultado = precoAlcool / precoGasolina
+            if( resultado >= 0.7){
+                //alert("Melhor utilizar Gasolina")
+                document.getElementById('resultado').innerHTML = "Melhor utilizar Gasolina"
+
+            }else{
+                //alert("Melhor utilizar Álcool")
+                document.getElementById('resultado').innerHTML = "Melhor utilizar Álcool"
+            }
+
+        }else{
+            alert("Preencha o preço da Gasolina")
+
+        }
+        
+    }else{
+        alert("Preencha o preço do Álcool")
+    }
+
+}
